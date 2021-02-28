c57de9b3cb5fb64de83bb0b5d5633762c4308bc0
lonetargaryen5 <swarnim.sonawane@gmail.com>
2021-02-28 23:31:00 +0530

    Final app.js fix.

--- API/app.js
+++ API/app.js
@@ -9,6 +9,15 @@ app.use(cors());
 
 app.use('/user', userRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
     console.log(`Server is up and running at http://localhost:${port}`)
   })
